7b3fe1a782bbf899e868a5a41c6870f72f252f0d
MiguelLopesDel <miguellopesdelmondes2@gmail.com>
2024-08-02 18:10:15 -0300

    handlers to DraconicMod and other mods

--- src/main/java/com/onepunchcrafts/util/TickUtilities.java
+++ src/main/java/com/onepunchcrafts/util/TickUtilities.java
@@ -1,6 +1,5 @@
 package com.onepunchcrafts.util;
 
-import com.onepunchcrafts.OnePunchCrafts;
 import com.onepunchcrafts.common.damage.DamagesRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
@@ -8,10 +7,13 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EndCrystal;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.AABB;
 
 import java.util.List;
+
+import static com.onepunchcrafts.OnePunchCrafts.DRACONIC_MOD;
 
 public class TickUtilities {
 
@@ -25,20 +27,34 @@ public class TickUtilities {
     //para isso posso pegar meu vetor e escalar ele.
     public boolean fillCylinderAndEmuleEffects(ServerPlayer player, final ServerLevel level, int breakBlocksPerTick, List<BlockPos> blocksPos) {
         int currentIteration = 0;
-        if(blocksPos.isEmpty() || startIndex >= blocksPos.size())
+        if (blocksPos.isEmpty() || startIndex >= blocksPos.size())
             return true;
         BlockPos pStart = blocksPos.get(startIndex);
         int i = 15;
         AABB pArea = new AABB(new BlockPos(pStart.getX() - i, pStart.getY() - i, pStart.getZ() - i),
                 new BlockPos(pStart.getX() + i, pStart.getY() + i, pStart.getZ() + i));
+        final DamageSource damageSource = new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamagesRegistry.SERIOUS_PUNCH_SECOND), null, player);
+        level.getEntitiesOfClass(EndCrystal.class, pArea).forEach(entity -> {
+            entity.setInvulnerable(false);
+            entity.setSecondsOnFire(60);
+            entity.hurt(damageSource, 10_000_000_000_000_000f);
+        });
+        if (DRACONIC_MOD.isPresent())
+            HelpUtility.hurtDraconicCrystals(level, pArea, damageSource);
         List<LivingEntity> entitiesOfClass = level.getEntitiesOfClass(LivingEntity.class, pArea);
         entitiesOfClass.forEach(entity -> {
             if (player.equals(entity))
                 return;
+            if (DRACONIC_MOD.isPresent()) {
+                if (isDraconicEntity(entity, damageSource)) return;
+            }
             entity.setInvulnerable(false);
             entity.setSecondsOnFire(60);
-            DamageSource damagesource = new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamagesRegistry.SERIOUS_PUNCH_SECOND), null, player);
-            entity.hurt(damagesource, 10_000_000_000_000_000f);
+            boolean hurt = entity.hurt(damageSource, 10_000_000_000_000_000f);
+            if (!hurt) {
+                entity.setLastHurtByPlayer(player);
+                entity.hurt(new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamagesRegistry.SERIOUS_PUNCH_SECOND), player, null), 10_000_000_000_000_000f);
+            }
         });
 
         for (int c = startIndex; c < blocksPos.size(); c++) {
@@ -54,5 +70,17 @@ public class TickUtilities {
                 return false;
         }
         return true;
+    }
+
+    private static boolean isDraconicEntity(LivingEntity entity, DamageSource damageSource) {
+        int crystalNum = HelpUtility.allowDamageInDraconicGuardian(entity);
+        if (crystalNum != -1) {
+            entity.setInvulnerable(false);
+            entity.setSecondsOnFire(60);
+            entity.hurt(damageSource, 10_000_000_000_000_000f);
+            HelpUtility.setCrystalToDefault(entity, crystalNum);
+            return true;
+        }
+        return false;
     }
 }
