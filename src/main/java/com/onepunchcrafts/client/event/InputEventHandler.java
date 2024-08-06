155072a4a987c837586e2e9e97e5438442750826
MiguelLopesDel <miguellopesdelmondes2@gmail.com>
2024-08-05 23:32:23 -0300

    somes skills are configured with mouse scrolling

--- src/main/java/com/onepunchcrafts/client/event/InputEventHandler.java
+++ src/main/java/com/onepunchcrafts/client/event/InputEventHandler.java
@@ -2,6 +2,7 @@ package com.onepunchcrafts.client.event;
 
 import com.onepunchcrafts.OnePunchCrafts;
 import com.onepunchcrafts.network.NetworkRegister;
+import com.onepunchcrafts.network.packet.PlayerSyncPacket;
 import com.onepunchcrafts.network.packet.SeriousPunchPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
@@ -20,6 +21,42 @@ public class InputEventHandler {
         player.getCapability(OnePunchCrafts.ONE_PLAYER_CAPABILITY).ifPresent(cap -> {
             if (cap.isSaitama() && cap.getActualAbility() == 2)
                 NetworkRegister.sendToServer(new SeriousPunchPacket());
+        });
+    }
+
+    @SubscribeEvent
+    public static void playerScroll(InputEvent.MouseScrollingEvent event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        double scrollDelta = event.getScrollDelta();
+        if (player == null || scrollDelta == 0) return;
+
+        player.getCapability(OnePunchCrafts.ONE_PLAYER_CAPABILITY).ifPresent(cap -> {
+            if (!cap.isSaitama())
+                return;
+            if (cap.getActualAbility() == 6) {
+                short speed = (short) (cap.getSpeed() + scrollDelta);
+                cap.setSpeed(speed < 0 ? 0 : speed);
+                NetworkRegister.sendToServer(new PlayerSyncPacket(cap));
+            } else if (cap.getActualAbility() == 8) {
+                short weight = (short) (cap.getWeight() + scrollDelta);
+                cap.setWeight(weight < 0 ? 0 : weight);
+                NetworkRegister.sendToServer(new PlayerSyncPacket(cap));
+            }
+            else if (cap.getActualAbility() == 9) {
+                short knockbackResistance = (short) (cap.getKnockbackResistance() + scrollDelta);
+                cap.setKnockbackResistance(knockbackResistance < 0 ? 0 : knockbackResistance);
+                NetworkRegister.sendToServer(new PlayerSyncPacket(cap));
+            }
+            else if (cap.getActualAbility() == 10) {
+                short attackKnockback = (short) (cap.getAttackKnockback() + scrollDelta);
+                cap.setAttackKnockback(attackKnockback < 0 ? 0 : attackKnockback);
+                NetworkRegister.sendToServer(new PlayerSyncPacket(cap));
+            }
+            else if (cap.getActualAbility() == 11) {
+                short swimSpeed = (short) (cap.getSwimSpeed() + scrollDelta);
+                cap.setSwimSpeed(swimSpeed < 0 ? 0 : swimSpeed);
+                NetworkRegister.sendToServer(new PlayerSyncPacket(cap));
+            }
         });
     }
 }
