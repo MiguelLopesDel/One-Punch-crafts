b333a65b30e168607b5d8e6a55b52316142da121
MiguelLopesDel <miguellopesdelmondes2@gmail.com>
2024-08-05 23:36:54 -0300

    Alert for the host player of an integrated server without the AttributeFix mod

--- src/main/java/com/onepunchcrafts/common/event/PlayerLoggedInEventHandler.java
+++ src/main/java/com/onepunchcrafts/common/event/PlayerLoggedInEventHandler.java
@@ -4,10 +4,19 @@ import com.onepunchcrafts.OnePunchCrafts;
 import com.onepunchcrafts.common.capability.OnePunchPlayer;
 import com.onepunchcrafts.network.NetworkRegister;
 import com.onepunchcrafts.network.packet.PlayerSyncPacket;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+
+import java.awt.*;
+
+import static java.awt.PageAttributes.ColorType.COLOR;
 
 @Mod.EventBusSubscriber
 public class PlayerLoggedInEventHandler {
@@ -15,6 +24,14 @@ public class PlayerLoggedInEventHandler {
     @SubscribeEvent
     public static void onPlayerLogging(PlayerEvent.PlayerLoggedInEvent event) {
         ServerPlayer player = (ServerPlayer) event.getEntity();
+        if (player.getServer().isSingleplayerOwner(player.getGameProfile()) && !ModList.get().isLoaded("attributefix")) {
+            ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.curseforge.com/minecraft/mc-mods/attributefix/files/all?page=1&pageSize=20");
+            HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Attributes fix site"));
+            MutableComponent msn = Component.literal("It is recommended to download the mod attributes fix, click here");
+            msn.setStyle(msn.getStyle().withClickEvent(clickEvent));
+            msn.setStyle(msn.getStyle().withHoverEvent(hoverEvent).withColor(Color.CYAN.getRGB()));
+            player.sendSystemMessage(msn);
+        }
         if (!player.level().isClientSide()) {
             OnePunchPlayer onePunchPlayer = player.getCapability(OnePunchCrafts.ONE_PLAYER_CAPABILITY).orElse(new OnePunchPlayer(false));
             NetworkRegister.sendToPlayer(player, new PlayerSyncPacket(onePunchPlayer));
